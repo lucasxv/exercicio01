3e74cbfc51bfab31fd6e6abb8c0aa37b93b217d5
lucas.bsilv16 <lucas.bsilv16@CAS419186C14709.senacsp.edu.br>
2015-08-14 20:06:14 -0300

    inserindo main

---
+++ src/main/java/br/senac/tads/pi3/lucasbarbosa/exercicio1/Exec01.java
@@ -0,0 +1,13 @@
+
+package br.senac.tads.pi3.lucasbarbosa.exercicio1;
+
+public class Exec01 {
+
+    public static void main (String[]args){
+    
+    
+        System.out.println("Hello");
+    }
+    
+    
+}
