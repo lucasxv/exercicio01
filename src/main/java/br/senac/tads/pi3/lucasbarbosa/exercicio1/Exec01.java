72a89cc030382ec7ee490f3ec20cbb2f8bac4ce9
lucas.bsilv16 <lucas.bsilv16@CAS419186C14709.senacsp.edu.br>
2015-08-14 22:09:56 -0300

    Formulario - Lucas

--- src/main/java/br/senac/tads/pi3/lucasbarbosa/exercicio1/Exec01.java
+++ src/main/java/br/senac/tads/pi3/lucasbarbosa/exercicio1/Exec01.java
@@ -1,12 +1,20 @@
 
 package br.senac.tads.pi3.lucasbarbosa.exercicio1;
 
-public class Exec01 {
+import java.util.Scanner;
 
+public class Exec01 {
+   
     public static void main (String[]args){
-    
-    
-        System.out.println("Hello");
+       Scanner sc = new Scanner(System.in);
+        String Nome;
+        String Telefone;
+        
+        System.out.println("Digite o Nome");
+        Nome = sc.nextLine();
+        
+        System.out.println("Digite o Telefone");
+        Telefone = sc.nextLine();
     }
     
     
